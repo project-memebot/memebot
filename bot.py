f2bd154020cfe957b364f3f5ad9c7ac0adeb7475
samsunghappytree123 <samsunghappytree123@naver.com>
2022-01-10 22:26:04 +0900

    feat(bot.py): basic task
    bot.py의 기본적인 작업입니다.

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import os
+import config
+import jishaku
+from discord.ext import commands
+
+if config.BOT.TEST_MODE:
+    prefix = config.BOT.TEST_PREFIX
+    token = config.BOT.TEST_TOKEN
+else:
+    prefix = config.BOT.BOT_PREFIX
+    token = config.BOT.BOT_TOKEN
+
+bot = commands.AutoShardedBot(command_prefix=prefix, intents=discord.Intents.default(), help_command=None, owner_ids=config.BOT.OWNER_IDS)
+
+bot.load_extension("jishaku")
+print("✅ | 로드 - Jishaku")
+for file in os.listdir("cogs/"):
+    if file.endswith(".py"):
+        try:
+            bot.load_extension(f"cogs.{file[:-3]}")
+            print(f"✅ | 로드 - cogs.{file[:-3]}")
+        except Exception as error:
+            print(f"❌ | 로드 실패 - cogs.{file[:-3]} ({error})")
+
+bot.run(token)
