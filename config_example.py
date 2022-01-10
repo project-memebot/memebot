2f79e71f7a67f19df5bc4fc915613cf66f224243
samsunghappytree123 <samsunghappytree123@naver.com>
2022-01-10 22:26:37 +0900

    feat(config): basic task
    config_example.py의 기본 작업입니다.

---
+++ config_example.py
@@ -0,0 +1,11 @@
+class BOT:
+    BOT_TOKEN = '실제 봇의 토큰을 입력해주세요.'
+    KOREANBOTS_TOKEN = '한국 디스코드 리스트의 봇 토큰을 입력해주세요.'
+    BOT_PREFIX = '실제 봇의 접두사를 입력해주세요.'
+    TEST_TOKEN = '테스트 봇의 토큰을 입력해주세요.'
+    TEST_PREFIX = '테스트 봇의 접두사를 입력해주세요.'
+    TEST_MODE = False
+    LOG_CHANNEL = '로그 채널 ID를 입력해주세요.'
+
+class DATABASE:
+    URI = "DATABASE(MONGODB)의 URI를 입력해주세요."
