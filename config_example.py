703c2ea1854f4d4562c88489841bbd500e81ae83
samsunghappytree123 <samsunghappytree123@naver.com>
2022-01-29 11:24:38 +0900

    fix(config): add owner_ids
    owner_ids가 없는 오류를 해결하였습니다.

--- config_example.py
+++ config_example.py
@@ -1,4 +1,5 @@
 class BOT:
+    OWNER_IDS = [] # 봇의 관리자 ID를 list 형식으로 입력해주세요.
     BOT_TOKEN = '실제 봇의 토큰을 입력해주세요.'
     KOREANBOTS_TOKEN = '한국 디스코드 리스트의 봇 토큰을 입력해주세요.'
     BOT_PREFIX = '실제 봇의 접두사를 입력해주세요.'
