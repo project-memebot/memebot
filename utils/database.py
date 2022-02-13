69a92a7eff5ed3511d82466cf54a1d819e19634a
samsunghappytree123 <samsunghappytree123@naver.com>
2022-02-13 16:45:40 +0900

    feat(utils.database): add insert_meme, etc...
    insert_meme을 추가하였으며, 코드를 수정하였어요.

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,7 @@
 import config
 import datetime
 import random
+import string
 import motor.motor_asyncio
 
 database = motor.motor_asyncio.AsyncIOMotorClient(config.DATABASE.URI).memebot
@@ -27,8 +28,8 @@ class USER_DATABASE:
         favorite_meme (str): 필수, 짤 ID 입력
         """
         user_data = await USER_DATABASE.user_find(user_id)
-        lists = [i["meme_id"] for i in user_data["favorite"]]
         if user_data:
+            lists = [i["meme_id"] for i in user_data["favorite"]]
             if favorite_meme in lists:
                 user_data["favorite"] = []
                 for i in user_data["favorite"]:
@@ -55,8 +56,8 @@ class USER_DATABASE:
         user_id (int): 필수, 디스코드 유저 ID 입력
         """
         user_data = await USER_DATABASE.user_find(user_id)
-        lists = [i["meme_id"] for i in user_data["favorite"]]
         if user_data:
+            lists = [i["meme_id"] for i in user_data["favorite"]]
             return {"code": 200, "favorite_list": lists, "message": "정상적으로 즐겨찾기 목록을 조회하였습니다."}
         else:
             return {"code": 403, "message": "가입을 진행하지 않았습니다. ``/가입`` 명령어로 가입이 필요합니다."}
@@ -131,18 +132,35 @@ class BLACKLIST:
 
 
 class MEME_DATABASE:
-    async def meme_list(filter: dict = None):
+    async def meme_list(filter: dict = {}):
         """
         filter (dict): 선택, DICT 형식으로 입력
         """
         meme_list = []
-        if filter:
-            async for i in database.meme.find(filter):
-                meme_list.append(i)
-        else:
-            async for i in database.meme.find({}):
-                meme_list.append(i)
+        async for i in database.meme.find(filter):
+            meme_list.append(i)
         return meme_list
+
+    async def insert_meme(title: str, url: str, uploader_id: int):
+        """
+        title (str): 필수, 짤 제목 입력
+        url (str): 필수, 짤 사진 url 입력
+        uploader_id (int): 필수, 짤 업로더 ID 입력
+        """
+        while True:
+            string_pool = string.ascii_letters + string.digits
+            randomcode = ""
+
+            for sul in range(8):
+                randomcode += random.choice(string_pool)
+
+            if (await MEME_DATABASE.find_meme(randomcode)) == None:
+                break
+
+            print(randomcode)
+
+        return await database.meme.insert_one({"_id": randomcode, "uploader_id": uploader_id, "title": title, "url": url, "upload_at": datetime.datetime.now(), "star":0})
+        #await database.user.insert_one({"_id": user_id, "created_at": datetime.datetime.now(), "favorite": []})
 
     async def random_meme():
         result = await MEME_DATABASE.meme_list()
@@ -150,8 +168,11 @@ class MEME_DATABASE:
 
     async def find_meme(query):
         result = database.meme.find({"_id": {"$regex": query}})
-        search_result = [i async for i in result]
-        return search_result[0]
+        try:
+            search_result = [i async for i in result]
+            return search_result[0]
+        except IndexError:
+            return None
 
 
 """
