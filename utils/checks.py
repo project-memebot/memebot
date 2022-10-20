45f859560aafe1ddac9261402bed04145340335a
samsunghappytree123 <samsunghappytree123@naver.com>
2022-10-20 17:04:44 +0900

    feat(utils.checks): add blacklist_check util

---
+++ utils/checks.py
@@ -0,0 +1,10 @@
+from utils.database import BLACKLIST
+from utils.embed import Embed
+
+async def blacklist_check(self):
+    if await BLACKLIST.search(self.author.id):
+        embed = Embed.ban_info(await BLACKLIST.search(self.author.id))
+        await self.respond(embed=embed, ephemeral=True)
+        return False
+    else:
+        return True
